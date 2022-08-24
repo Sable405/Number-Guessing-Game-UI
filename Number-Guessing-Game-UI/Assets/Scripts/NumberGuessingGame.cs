62e158eed2415929d156fd440079559a83e7c5f6
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-08-24 11:01:41 -0700

    Updating Start Game Scene

--- Number-Guessing-Game-UI/Assets/Scripts/NumberGuessingGame.cs
+++ Number-Guessing-Game-UI/Assets/Scripts/NumberGuessingGame.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Number-Guessing-Game : MonoBehaviour
+public class NumberGuessingGame : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Number-Guessing-Game-UI/Assets/Scripts/SceneLoader.cs
+++ Number-Guessing-Game-UI/Assets/Scripts/SceneLoader.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scene-Loader : MonoBehaviour
+public class WinScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
