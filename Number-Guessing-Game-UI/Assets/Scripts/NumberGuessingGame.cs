73270f2e80d9949673d5f1af61f3c02b1aea1bb9
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-08-31 11:01:57 -0700

    All done :3

--- Number-Guessing-Game-UI/Assets/Scripts/NumberGuessingGame.cs
+++ Number-Guessing-Game-UI/Assets/Scripts/NumberGuessingGame.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class NumberGuessingGame : MonoBehaviour
+public class NumberGuessingGame: MonoBehaviour
 {
-   [SerializedField] public int guess = 5; 
-   [SerializedField] public int minValue = 1; 
-   [SerializedField] public int maxValue = 10;
+private int guess; 
+   [SerializeField] private int minValue = 1; 
+   [SerializeField] private int maxValue = 10;
+   [SerializeField] private TextMeshProUGUI guessText;
 
     // Start is called before the first frame update
     void Start()
@@ -29,26 +31,22 @@ public class NumberGuessingGame : MonoBehaviour
     }
     public void OnPressHigher()
     {
-            minValue = guess; 
-            guess = (maxValue + minValue) / 2;
-            Debug.Log("Is thine number higher or lower than: " + guess);
+           minValue = guess + 1 ; 
+           NextGuess();
     }
     public void OnPressLower()
     {
-maxValue = guess;
-            guess = (maxValue + minValue) / 2;
-            Debug.Log("Is the majesty's numbah higher or lower than: " + guess);
+        maxValue = guess - 1 ;
+        NextGuess();
     }
     public void NextGuess()
     {
-
+        guess = Random.Range(minValue, maxValue + 1);
+        // code
+        guessText.text = guess.ToString();
     }
     void StartGame()
     {
-        guess = 5;
-        minValue = 1;
-        maxValue = 10;
-    
-        maxValue = maxValue + 1; 
+     NextGuess();
     }
-}
+}  
